693d4686d88d3a0eaf4747e310d405cd0533016d
Dr sOuL <shivamscientist2004@gmail.com>
2026-06-02 09:54:53 +0530

    fix: import paths for auth

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { signIn } from "../../../../lib/auth/auth";
+import { signIn } from "@/lib/auth/auth";
 import { AuthError } from "next-auth";
 
 export async function loginAction(prevState: any, formData: FormData) {
